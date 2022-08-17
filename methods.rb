09c12781667f8296b1630ea35448f5cecd522e14
oscar kiprop chemonges <oscar.chemonges@student.moringaschool.com>
2022-08-17 14:33:01 +0300

    done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
-# Your code here!
+# # Your code here!
+def  greet_programmer
+    puts "Hello, programmer!"
+end 
+
+def greet(name = "Naureen")
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "Sunny")
+    puts "Hello, #{name}!"
+    puts "Hello, programmer!"
+end 
+
+def add(num1,num2)
+    return num1 + num2;
+end 
+sum = add(1, 2)
+
+def halve(number)
+    if number.class != Integer
+        return nil
+    end
+    
+    number/2
+end
+
+halve(4)
+halve('two')
